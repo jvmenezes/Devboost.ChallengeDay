3e87ee8451466b2e937865b3aef250816336901e
Andre Soares <andresoares37@gmail.com>
2020-09-24 20:23:38 -0300

    Criando a classe do Swagger

--- Devboost.ChallengeDay/Startup.cs
+++ Devboost.ChallengeDay/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Devboost.ChallengeDay.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace Devboost.ChallengeDay
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.SwaggerAdd();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
